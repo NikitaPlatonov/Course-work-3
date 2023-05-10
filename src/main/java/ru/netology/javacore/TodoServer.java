a4d8c6850d4d79965734e6383793ea7e6cbf660e
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-10 21:24:46 +0300

    настроил сервер

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -3,7 +3,10 @@ package ru.netology.javacore;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,27 +15,27 @@ public class TodoServer {
 
     public TodoServer(int port, Todos todos) {
         this.port = port;
-        try (ServerSocket serverSocket = new ServerSocket(port);) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен");
             while (true) {
-                try (Socket socket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream());) {
+                try (Socket socket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+                    System.out.println("Подключился клиент с таким портом: " + socket.getPort());
                     out.println("Привет! Я жду твой запрос в json-формате");
-                    while (true){
-                        String strJson = in.readLine();
-                        JsonParser parser = new JsonParser();
-                        JsonObject jsonObject = parser.parse(strJson).getAsJsonObject();
-                        String method = jsonObject.get("type").getAsString();
-                        String task = jsonObject.get("task").getAsString();
-                        if(method.equals("addTask")){
-                            todos.addTask(task);
-                            out.println("Измененный список: ");
-                            out.println(todos.getAllTasks());
-                        } else if(method.equals("removeTask")){
-                            todos.removeTask(task);
-                            out.println("Измененный список: ");
-                            out.println(todos.getAllTasks());
-                        } else{
-                            out.println("Такой функции нет");
-                        }
+                    String strJson = in.readLine();
+                    JsonParser parser = new JsonParser();
+                    JsonObject jsonObject = parser.parse(strJson).getAsJsonObject();
+                    String methodFromRequest = jsonObject.get("type").getAsString();
+                    String taskFromRequest = jsonObject.get("task").getAsString();
+                    if (methodFromRequest.equals("ADD")) {
+                        todos.addTask(taskFromRequest);
+                        out.println("Измененный список: ");
+                        out.println(todos.getAllTasks());
+                    } else if (methodFromRequest.equals("REMOVE")) {
+                        todos.removeTask(taskFromRequest);
+                        out.println("Измененный список: ");
+                        out.println(todos.getAllTasks());
+                    } else {
+                        out.println("Такой функции нет");
                     }
                 }
             }
