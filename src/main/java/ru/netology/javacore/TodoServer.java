1da07f661a7d1b77f819c2e1a37374f8f6608717
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-10 20:22:37 +0300

    реализовал класс TodoServer

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,21 +1,48 @@
 package ru.netology.javacore;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private final int port;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+            while (true) {
+                try (Socket socket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream());) {
+                    out.println("Привет! Я жду твой запрос в json-формате");
+                    while (true){
+                        String strJson = in.readLine();
+                        JsonParser parser = new JsonParser();
+                        JsonObject jsonObject = parser.parse(strJson).getAsJsonObject();
+                        String method = jsonObject.get("type").getAsString();
+                        String task = jsonObject.get("task").getAsString();
+                        if(method.equals("addTask")){
+                            todos.addTask(task);
+                            out.println("Измененный список: ");
+                            out.println(todos.getAllTasks());
+                        } else if(method.equals("removeTask")){
+                            todos.removeTask(task);
+                            out.println("Измененный список: ");
+                            out.println(todos.getAllTasks());
+                        } else{
+                            out.println("Такой функции нет");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
     }
 }
