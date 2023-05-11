00f086ab1e3ebf3f9c6ca0767af04dcaa2566a2b
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-11 13:35:29 +0300

    написал тест addTaskMoreSeven

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,29 @@
 package ru.netology.javacore;
 
-public class TodosTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    // ваши тесты для класса Todos
+public class TodosTests {
+    Todos todos;
+    @BeforeEach
+    void init(){
+        todos = new Todos();
+    }
+    @Test
+    //Этот тестирует то, что метод addTask в классе Todos не будет добавлять никакие задачи после 7-й
+    void addTaskMoreSeven(){
+        todos.addTask("Первая задача");
+        todos.addTask("Вторая задача");
+        todos.addTask("Третья задача");
+        todos.addTask("Четвертая задача");
+        todos.addTask("Пятая задача");
+        todos.addTask("Шестая задача");
+        todos.addTask("Седьмая задача");
+        todos.addTask("Восьмая задача");
+        todos.addTask("Девятая задача");
+        todos.addTask("Десятая задача");
+        String expected = "Вторая задача Первая задача Пятая задача Седьмая задача Третья задача Четвертая задача Шестая задача ";
+        Assertions.assertEquals(expected, todos.getAllTasks());
+    }
 }
