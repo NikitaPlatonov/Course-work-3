293a33ab4dcd77dea7f490adce6dbd76694cdce0
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-11 12:57:23 +0300

    настроил сравнение задач через compareToIgnoreCase

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,10 +4,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    private static final int SIZE_TODOLIST_ARRAY = 7;
     private static Set<String> tasks = new HashSet<>();
     public void addTask(String task) {
-        if(tasks.size() < 8 && task != null){
+        if(tasks.size() < 7){
             tasks.add(task);
         }
         getAllTasks();
@@ -20,7 +19,7 @@ public class Todos {
 
     public String getAllTasks() {
         List<String> sortedListTasks = new ArrayList<>(tasks);
-        Collections.sort(sortedListTasks);
+        sortedListTasks.sort(String::compareToIgnoreCase);
         StringBuilder allTasks = new StringBuilder();
         for(String task : sortedListTasks){
             allTasks.append(task).append(" ");
